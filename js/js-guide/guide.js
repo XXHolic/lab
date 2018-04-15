30238cfd31e3d318ac576713fb69624ec8135246
thy_dell <972143891@qq.com>
2018-04-15 17:08:57 +0800

    write guide.js

--- js/js-guide/guide.js
+++ js/js-guide/guide.js
@@ -56,7 +56,7 @@
   function functionForEach(arr, fuc) {
     if (arr) {
       for (var i = 0, len = arr.length; i < len; i++) {
-        fnc(arr[i], i);
+        fuc(arr[i], i);
       }
     }
   }
@@ -80,7 +80,7 @@
    * @param {Object} targetEle
    */
   function guideInit(targetEle) {
-    var allGuideSteps = targetEle.querySelector('*[data-intro]'),
+    var allGuideSteps = targetEle.querySelectorAll('*[data-intro]'),
         guideItems = [];
     var eleLen = allGuideSteps.length;
     // 没有引导元素，就停止执行
@@ -117,11 +117,25 @@
    * @param {Object} targetEle
    */
   function showElement(targetEle) {
-    console.info("创建元素");
+    console.info("targetEle",targetEle);
     var self = this,
-        guideContainer = document.querySelector('.guide-container');
+        guideContainer = document.querySelector('.guide-container'),
+        nextButton,
+        prevButton,
+        skipButton;
 
     if (guideContainer !== null) {
+      var guideTipEle = guideContainer.querySelector('.guide-tip');
+      var guideTipTextEle = guideContainer.querySelector('.guide-tip-text');
+
+      guideTipEle.style.opacity = 0;
+      // guideTipEle.style.display = 'none';
+
+      guideTipTextEle.innerText = targetEle.intro;
+
+      setGuideLayerPosition.call(self,guideContainer);
+
+      guideTipEle.style.opacity = 1;
 
     } else {
       var guideLayer = document.createElement('div');
@@ -132,12 +146,46 @@
       guideLayer.className = 'guide-container';
       guideTipLayer.className = 'guide-tip';
       guideTipTextLayer.className = 'guide-tip-text';
-      guideTipBtnLayer.className = 'guide-tip-btns';
 
-      guideLayer.appendChild(guideTipLayer);
+      //提示文字显示的元素
+      guideTipTextLayer.innerText = targetEle.intro;
+
+      // 创建按钮
+      guideTipBtnLayer.className = 'guide-tip-btns';
+      skipButton = document.createElement('button');
+      skipButton.innerText = this.options.skipText;
+      skipButton.onclick = function() {
+        exitGuide.call(self);
+      };
+      guideTipBtnLayer.appendChild(skipButton);
+
+      preButton = document.createElement('button');
+      preButton.innerText = this.options.prevText;
+      preButton.onclick = function() {
+        if (self.currentStep !== 0) {
+          previousStep.call(self);
+        }
+        
+      };
+      guideTipBtnLayer.appendChild(preButton);
+
+      nextButton = document.createElement('button');
+      nextButton.innerText = this.options.nextText;
+      nextButton.onclick = function() {
+        console.info('next');
+        if (self.allGuideItems.length-1 !== self.currentStep) {
+          nextStep.call(self);
+        }
+        
+      };
+      guideTipBtnLayer.appendChild(nextButton);
+
+      // 前面各子元素组装好后，组合到一起
       guideTipLayer.appendChild(guideTipTextLayer);
       guideTipLayer.appendChild(guideTipBtnLayer);
-
+      guideLayer.appendChild(guideTipLayer);
+      
+      // 对提示进行定位
       setGuideLayerPosition.call(self,guideLayer);
       this.targetElement.appendChild(guideLayer);
 
@@ -148,11 +196,69 @@
   // 设置引导层阴影的位置
   function setGuideLayerPosition(guideLayer) {
     if (guideLayer) {
-      var elementPosition = getOffset();
-      guideLayer.style.cssText = '';
+      var currentGuideItem = this.allGuideItems[this.currentStep];
+      if (!currentGuideItem) {
+        return;
+      }
+      var elementPosition = getOffset(currentGuideItem.element);
+      var windowsWidth = document.documentElement.clientWidth;
+      var windowsHeight = document.documentElement.clientHeight;
+      var borderRightWidth = windowsWidth - elementPosition.width - elementPosition.left;
+      var borderBottomWidth = windowsHeight - elementPosition.height - elementPosition.top;
+      guideLayer.style.cssText = 
+        'width:' + elementPosition.width + 'px;' + 
+        'height:' + elementPosition.height + 'px;' + 
+        'border-width:' + elementPosition.top + 'px ' + borderRightWidth + 'px ' + borderBottomWidth + 'px ' + elementPosition.left + 'px;'
+        ;
     }
   }
 
+  // 下面是事件触发的方法
+  // 下一步
+  function nextStep() {
+    this.direction = 'forward';
+
+    // if (this.currentStep !== 0) {
+    ++this.currentStep;
+    // }
+
+    var nextStepItem = this.allGuideItems[this.currentStep];
+    // var continueStep = true;
+
+    // 最后一步
+    if (this.allGuideItems.length <= this.currentStep) {
+      return;
+    }
+
+    showElement.call(this,nextStepItem);
+  }
+
+  // 上一步
+  function previousStep() {
+    this.direction = 'backward';
+    
+    if (this.currentStep === 0) {
+      return;
+    }
+
+    --this.currentStep;
+    var preStepItem = this.allGuideItems[this.currentStep];
+
+    showElement.call(this,preStepItem);
+
+  }
+
+  // 跳过和完成
+  function exitGuide() {
+    var guideContainer = document.querySelector('.guide-container');
+    document.body.removeChild(guideContainer);
+    // document.removeChild(guideContainer);
+    this.currentStep = 0;
+  }
+
+
+
+  // 这个是对外暴露的对象
   var guide = function (targetEle) {
     var instance = null;
 
@@ -179,6 +285,7 @@
   // 这个fn是为了方便理解才存在的，其实没有什么作用
   guide.fn = Guide.prototype = {
     start: function() {
+      guideInit.call(this,this.targetElement);
       console.info("test is success");
     }
   };
