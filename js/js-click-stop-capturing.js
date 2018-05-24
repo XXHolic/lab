2b029bf6361d71789395f1ca1723d5e6e1b9158b
thy_dell <972143891@qq.com>
2018-05-24 23:20:19 +0800

    feat: add new test for 14.draft

---
+++ js/js-click-stop-capturing.js
@@ -0,0 +1,65 @@
+window.onload = function() {
+  var capturingEle = document.getElementById('clickCapturing');
+  var capturingParentEle = document.getElementById('clickCapturingParent');
+  var bodyEle = document.getElementById('body');
+  var HtmlEle = document.getElementById('html');
+  var showCapturingEle = document.getElementById('showCapturingEle');
+
+  function getRandomNum(len) {
+    var numString = String(Math.random());
+    if (len) {
+      return numString.substring(0,len);
+    } else {
+      return numString;
+    }
+  }
+
+  function showDeviceType() {
+    var deviceShowEle = document.getElementById('deviceShow');
+    var deviceType = Util.getDeviceType();
+    var deviceTypeTxt = deviceType==='pc'?'PC端':'手机端';
+    deviceShowEle.innerText = deviceTypeTxt;
+  }
+
+  function createShowEle(text) {
+    var divEle = document.createElement('div');
+    divEle.innerText = text;
+    showCapturingEle.appendChild(divEle);
+  }
+
+  showDeviceType();
+
+  Util.Event.addHandler(capturingEle, 'click', function(e) {
+    capturingEle.innerText = '已点击，参照数：'+getRandomNum(8);
+    var showText = '点击元素 capturing，参照数：'+getRandomNum(8);
+    createShowEle(showText);
+  },true);
+
+  Util.Event.addHandler(capturingParentEle, 'click', function(e) {
+    e.stopPropagation();
+    var showText = '父元素 capturing，参照数：'+getRandomNum(8);
+    createShowEle(showText);
+  },true);
+
+  Util.Event.addHandler(bodyEle, 'click', function(e) {
+    var showText = 'body 元素 capturing，参照数：'+getRandomNum(8);
+    createShowEle(showText);
+  },true);
+
+  Util.Event.addHandler(HtmlEle, 'click', function(e) {
+    var showText = 'html 元素 capturing，参照数：'+getRandomNum(8);
+    createShowEle(showText);
+  },true);
+
+  Util.Event.addHandler(document, 'click', function (e) {
+    var showText = 'document capturing ，参照数：' + getRandomNum(8);
+    createShowEle(showText);
+  }, true);
+
+  Util.Event.addHandler(window, 'click', function(e) {
+    var showText = 'window capturing，参照数：'+getRandomNum(8);
+    createShowEle(showText);
+  },true);
+
+}
+
