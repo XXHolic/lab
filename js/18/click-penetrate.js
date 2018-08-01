a2cfd5c9abe618767d1496de4fede1b54c130573
thy <972143891@qq.com>
2018-08-01 09:48:22 +0800

    feat: write test

--- js/18/click-penetrate.js
+++ js/18/click-penetrate.js
@@ -1,7 +1,18 @@
 function pageInit() {
+  var clickEle1 = document.getElementById('clickButton1');
+  Util.Event.addHandler(clickEle1, 'click', function () {
+    clickEle1.innerText = '已点击，参照：' + Util.getRandomNum(8);
+  });
 
+  var clickEle2 = document.getElementById('clickButton2');
+  Util.Event.addHandler(clickEle2, 'click', function () {
+    clickEle2.innerText = '已点击，参照：' + Util.getRandomNum(8);
+  });
 
+  var shadowEle = document.getElementById('shadowEle');
+  Util.Event.addHandler(shadowEle, 'click', function () {
 
+  });
 }
 
 pageInit();
