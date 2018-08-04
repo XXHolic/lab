15350395d0c6d4a81e5e40c8e692a4330a7879f1
thy_dell <972143891@qq.com>
2018-08-04 23:36:37 +0800

    refactor: write test

--- js/18/click-penetrate.js
+++ js/18/click-penetrate.js
@@ -1,17 +1,21 @@
 function pageInit() {
   var clickEle1 = document.getElementById('clickButton1');
   Util.Event.addHandler(clickEle1, 'click', function () {
-    clickEle1.innerText = '已点击，参照：' + Util.getRandomNum(8);
+    clickEle1.innerText = '已点击，参照：' + Util.getRandomNum(4);
+  });
+  var shadowEle = document.getElementById('shadowEle');
+  Util.Event.addHandler(shadowEle, 'touchstart', function () {
+    shadowEle.style.display="none";
+  });
+
+  var shadow2Ele = document.getElementById('shadowEle2');
+  Util.Event.addHandler(shadow2Ele, 'click', function () {
+    shadow2Ele.style.display="none";
   });
 
   var clickEle2 = document.getElementById('clickButton2');
   Util.Event.addHandler(clickEle2, 'click', function () {
-    clickEle2.innerText = '已点击，参照：' + Util.getRandomNum(8);
-  });
-
-  var shadowEle = document.getElementById('shadowEle');
-  Util.Event.addHandler(shadowEle, 'touchstart', function () {
-    shadowEle.style.display="none";
+    clickEle2.innerText = '已点击，参照：' + Util.getRandomNum(4);
   });
 }
 
