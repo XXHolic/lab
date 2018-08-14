070a260895a70ea85d10a60965a47b2bdbf6d53b
thy <972143891@qq.com>
2018-08-14 18:41:08 +0800

    refactor: write test for 9.md in segment file

--- js/segment/9.create-event.js
+++ js/segment/9.create-event.js
@@ -1,10 +1,49 @@
 window.onload = function () {
-  var btn = document.getElementById('click');
-  Util.Event.addHandler(btn, 'click', function () {
-    alert('clicked');
+
+  function sendClickOldFashion() {
+    var clickEvent = document.createEvent('MouseEvents');
+    clickEvent.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0,false, false, false, false, 0, null);
+    var  checkboxEle = document.getElementById('checkbox');
+    checkboxEle.dispatchEvent(clickEvent);
+  }
+
+  function sendClickNewFashion() {
+    var clickEvent = new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true,
+      view: window,
+      detail: 0,
+      screenX: 0,
+      screenY: 0,
+      clientX: 0,
+      clientY: 0,
+      ctrlKey: false,
+      altKey: false,
+      shiftKey: false,
+      metaKey: false,
+      button: 0,
+      relatedTarget: null
+    });
+    var checkboxEle = document.getElementById('checkbox');
+    checkboxEle.dispatchEvent(clickEvent);
+  }
+
+  var clickOldEle = document.getElementById('clickOld');
+  Util.Event.addHandler(clickOldEle, 'click', function () {
+    try {
+      sendClickOldFashion();
+    } catch (error) {
+      alert("不支 document.createEvent() 的方式");
+    }
   });
 
-  var clickEvent = document.createEvent('MouseEvents');
-  clickEvent.initMouseEvent("click", true, true, document.defaultView, 0, 0, 0, 0, 0,false, false, false, false, 0, null);
-  btn.dispatchEvent(clickEvent);
+  var clickNewEle = document.getElementById('clickNew');
+  Util.Event.addHandler(clickNewEle, 'click', function () {
+    try {
+      sendClickNewFashion();
+    } catch (error) {
+      alert("不支持构造函数的方式");
+    }
+  });
+
 }
