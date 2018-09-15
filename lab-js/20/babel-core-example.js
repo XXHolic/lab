bae10688a24019a7a92acd9848c185925e02f4a9
thy <972143891@qq.com>
2018-09-15 19:45:03 +0800

    feat: add babel

--- lab-js/20/babel-core-example.js
+++
@@ -1,9 +0,0 @@
-var babel = require("@babel/core");
-var code = "()=>{let a=3;a++}";
-babel.transform(code, {},function (err, result) {
-  console.info('ddd');
-  console.info('code',result.code);
-  console.info('result', result.map);
-  console.info('result', result.ast);
-  // result; // => { code, map, ast }
-});
---
+++ lab-js/20/src/babel-core-local.js
@@ -0,0 +1,3 @@
+const getName = (name) => {
+  return name;
+};
