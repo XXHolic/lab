aecb9a59113cbf84da01b2187aff9c11127b01ac
thy <972143891@qq.com>
2019-01-20 19:13:17 +0800

    feat: add request type

--- lab-js/segment-31/binary-to-base64.js
+++ lab-js/segment-31/binary-to-base64.js
@@ -1,12 +1,13 @@
 window.onload = function() {
-  var showDataObj = document.getElementById('showData');
+  var showImageObj = document.getElementById('showImage');
 
   var xhr = new XMLHttpRequest();
-  xhr.open('post','https://xxholic.github.io/lab/images/html5-rocks.png');
-  xhr.responseType = 'blob';
+  xhr.open('get','https://xxholic.github.io/lab/images/logo.jpg');
+  xhr.responseType = 'blob'; // 安卓4.3的不支持
+  // xhr.responseType = 'arraybuffer';
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4) {
-      showDataObj.innerText = String(xhr.response);
+      showImageObj.src = URL.createObjectURL(xhr.response);
     }
   }
   xhr.send();
