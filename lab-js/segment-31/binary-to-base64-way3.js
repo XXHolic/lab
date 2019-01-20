78ae0e006c26ed84215275d3070c8ca24c7a9ca1
thy <972143891@qq.com>
2019-01-20 23:39:31 +0800

    feat: add test for 31.md in segment

---
+++ lab-js/segment-31/binary-to-base64-way3.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+  var showImageObj = document.getElementById('showImage');
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('get','https://xxholic.github.io/lab/images/logo.jpg');
+  xhr.responseType = 'blob'; // 安卓4.3的不支持
+  // xhr.responseType = 'arraybuffer';
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      // showImageObj.src = URL.createObjectURL(xhr.response);
+      var reader = new FileReader();
+      reader.readAsDataURL(xhr.response);
+      reader.onload = function (e) {
+          console.info(reader.result);
+          showImageObj.src = reader.result;
+      }
+
+    }
+  }
+  xhr.send();
+};
