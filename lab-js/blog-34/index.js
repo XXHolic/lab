0ad60b7a4af5dd798d76b779c0d661621965ea54
thy <972143891@qq.com>
2019-06-09 15:09:45 +0800

    feat: add new test for 34.md in blog

--- lab-js/blog-34/index.js
+++ lab-js/blog-34/index.js
@@ -1,19 +1,60 @@
 window.onload = function() {
-
-  var getData = function () {
-    var xhr = new XMLHttpRequest();
-
-    xhr.open('GET','./data.json');
-
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState == 4) {
-        console.info(xhr.responseText);
-      }
+  const promise = new Promise(function(resolve, reject) {
+    if (true) {
+      resolve("resolve value");
+    } else {
+      reject("reject value");
     }
+  });
 
-    xhr.send();
-  }
+  console.info("promise:", promise);
 
-  getData();
+  var getJSONData = function(url) {
+    const promise = new Promise(function(resolve, reject) {
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState !== 4) {
+          return;
+        }
 
-}
+        if (xhr.status === 200) {
+          resolve(JSON.parse(xhr.responseText));
+        } else {
+          reject(new Error(xhr.statusText));
+        }
+      };
+
+      xhr.open("GET", url);
+      xhr.send();
+    });
+
+    return promise;
+  };
+
+  var thenResult = getJSONData("./data.json").then(
+    function(json) {
+      console.info("jsonData:", json);
+    },
+    function(error) {
+      console.error("出错了", error);
+    }
+  );
+
+  console.info("thenResult:", thenResult);
+
+  const doAsyncThing = function() {
+    return new Promise(function(resolve, rejected) {
+      resolve(x);
+    });
+  };
+
+  doAsyncThing().then(function(value) {
+    console.info(value);
+  });
+
+  setTimeout(() => {
+    console.info(222);
+  }, 1000);
+
+
+};
