5ce424758f1f0eba205dd0d9f49d20e580048bf0
thy <972143891@qq.com>
2019-07-03 22:11:53 +0800

    refactor: write code

--- lab-js/segment-43/index.js
+++ lab-js/segment-43/index.js
@@ -2,18 +2,30 @@ var fs = require("fs");
 var path = require("path");
 
 function readDir(dir) {
+  var exist = fs.existsSync(dir);
+  if (!exist) {
+    console.error("目录路径不存在");
+    return;
+  }
   var pa = fs.readdirSync(dir);
-  pa.forEach(function(file,index) {
-    console.warn(index)
-    var pathName = path.join(dir,file);
+
+  for (let index = 0; index < pa.length; index++) {
+    let file = pa[index];
+    var pathName = path.join(dir, file);
     var info = fs.statSync(pathName);
-    if(info.isDirectory()) {
+    if (info.isDirectory()) {
       console.info("dir:" + file);
       readDir(pathName);
     } else {
+      if (path.extname(file) === '.json') {
+        console.info('get it');
+
+      }
       console.info("file:" + file);
     }
-  })
+  }
+
+
 }
 
-readDir("/Users/thy/repository/my/lab/css/");
+readDir("/Users/thy/repository/my/lab/css");
