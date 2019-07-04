d7a7a2d4d5d0612c8b09ef07c2f29f9893ef2f00
thy <972143891@qq.com>
2019-07-04 23:14:35 +0800

    refactor: write logic

--- lab-js/segment-43/index.js
+++ lab-js/segment-43/index.js
@@ -1,5 +1,10 @@
 var fs = require("fs");
 var path = require("path");
+var currentPath = process.cwd();// 获取当前执行路径
+
+console.info("process.cwd()", process.cwd());
+
+var fileArr=[];
 
 function readDir(dir) {
   var exist = fs.existsSync(dir);
@@ -14,18 +19,56 @@ function readDir(dir) {
     var pathName = path.join(dir, file);
     var info = fs.statSync(pathName);
     if (info.isDirectory()) {
-      console.info("dir:" + file);
+      // console.info("dir:" + file);
       readDir(pathName);
     } else {
       if (path.extname(file) === '.json') {
-        console.info('get it');
-
+        // console.info('get it');
+        fileArr.push(pathName);
       }
-      console.info("file:" + file);
+      // console.info("file:" + file);
     }
   }
 
 
 }
 
-readDir("/Users/thy/repository/my/lab/css");
+/**
+ *
+ * @param {Array} arr 包含了所有 JSON 文件的路径
+ * @returns {String} 返回合并后 JSON 字符串
+ */
+function combineFile(arr) {
+  var obj = {};
+  arr.length && arr.forEach(ele => {
+    // console.info("deleDom:", element);
+    var str = deleDom(ele);
+    var contentObj = JSON.parse(str);
+    Object.assign(obj,contentObj);
+  });
+  return JSON.stringify(obj);
+}
+
+// 删除 dom 符号，防止异常
+function deleDom(filePath) {
+  var bin = fs.readFileSync(filePath);
+  if (bin[0]=== 0xEF && bin[1] === 0xBB && bin[2]=== 0xBF) {
+    bin = bin.slice(3);
+  }
+
+  return bin.toString('utf-8');
+}
+
+readDir(currentPath);
+
+var jsonStr = combineFile(fileArr);
+
+fs.writeFile('./data.json',jsonStr,function(err) {
+  if (err) {
+    console.error('文件写入失败');
+  } else {
+    console.info("文件写入成功，路径为：", currentPath);
+  }
+});
+
+
