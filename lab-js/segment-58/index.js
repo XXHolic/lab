1c2c101ab86714cf3d25a854965261aec0f9f17c
thy <972143891@qq.com>
2019-12-28 20:36:02 +0800

    refactor: write test for 58.md in segment

--- lab-js/segment-58/index.js
+++ lab-js/segment-58/index.js
@@ -1,8 +1,6 @@
 window.onload = function () {
-  // console.info(typeof document.body.style.width);
-  // console.info(typeof document.body.style.aa);
 
-  function testCSS() {
+  function isSupportCSS() {
 
     console.info(typeof document.body.style.width)
     console.info(typeof document.body.style.test)
@@ -13,120 +11,107 @@ window.onload = function () {
     console.info(CSS.supports("display", "test"));
   }
 
-  function testEvent(eventName) {
+
+
+  function isSupportEvent(eventName) {
+    if (typeof eventName !== 'string') {
+      console.log('Event name is not legal !');
+      return;
+    }
     var element = document.createElement('div');
     eventName = 'on' + eventName;
-    var isSupport = eventName in element;
-    console.info('isSupport', isSupport);
+    var isSupport = Boolean(eventName in element);
+    return isSupport;
   }
 
-  function testWindowObject() {
-    console.info('isSupport localStorage', window.localStorage);
+  var isSupportClick = isSupportEvent('click');
+  var isSupportCustom = isSupportEvent('custom');
+  console.info('isSupportClick:',isSupportClick);
+  console.info('isSupportCustom:',isSupportCustom);
 
-
-    console.info('isSupport formData', window.FormData);
-
-    console.info('isSupport indexedDB', window.indexedDB);
-    console.info('isSupport Worker', new Worker());
+  function isSupportObject(objName) {
+    if (typeof objName !== 'string') {
+      console.log('Object name is not legal !');
+      return;
+    }
+    return Boolean(window[objName]);
   }
 
-  function testApi() {
-    if (window.FormData) {
-      var myForm = document.getElementById('myForm');
-      var formData = new FormData(myForm);
-      console.info('isSupport formData.has', formData.has);
+  var isSupportLocalStorage = isSupportObject('localStorage');
+  console.info('isSupportLocalStorage:',isSupportLocalStorage);
+  var isSupportIndexedDB = isSupportObject('indexedDB');
+  console.info('isSupportIndexedDB:',isSupportIndexedDB);
+  var isSupportWorker = isSupportObject('Worker');
+  console.info('isSupportWorker:',isSupportWorker);
+
+//   var isSupportFontFamily = function (f) {
+//     if (typeof f != "string") {
+//         return false
+//     }
+//     var h = "Arial";
+//     if (f.toLowerCase() == h.toLowerCase()) {
+//         return true
+//     }
+//     var e = "a";
+//     var d = 100;
+//     var a = 100,
+//         i = 100;
+//     var c = document.createElement("canvas");
+//     var b = c.getContext("2d");
+//     c.width = a;
+//     c.height = i;
+//     b.textAlign = "center";
+//     b.fillStyle = "black";
+//     b.textBaseline = "middle";
+//     var g = function (j) {
+//         b.clearRect(0, 0, a, i);
+//         b.font = d + "px " + j + ", " + h;
+//         b.fillText(e, a / 2, i / 2);
+//         var k = b.getImageData(0, 0, a, i).data;
+//         return [].slice.call(k).filter(function (l) {
+//             return l != 0
+//         })
+//     };
+//     return g(h).join("") !== g(f).join("")
+// }
+
+  function isSupportFontFamily(font) {
+    if (typeof font !== 'string') {
+      console.log('Font name is not legal !');
+      return;
     }
 
-  }
+    var width;
+    var body = document.body;
 
-  function checkFont1(strFamily) {
-    var objDiv = document.createElement('div');
+    var container = document.createElement('span');
+    container.innerHTML = Array(10).join('wi');
+    container.style.cssText = [
+      'position:absolute',
+      'width:auto',
+      'font-size:128px',
+      'left:-99999px'
+    ].join(' !important;');
 
-    objDiv.style.fontFamily = strFamily;
-    objDiv.appendChild(document.createTextNode('FONT TEST'));
+    var getWidth = function (fontFamily) {
+      container.style.fontFamily = fontFamily;
+      body.appendChild(container);
+      width = container.clientWidth;
+      body.removeChild(container);
 
-    if (window.getComputedStyle) {
-      return window.getComputedStyle(objDiv, null).getPropertyValue('font-family') === strFamily;
-    }
-
-    return objDiv.currentStyle.fontFamily === strFamily;
-  }
-
-  var isSupportFontFamily = function (f) {
-    if (typeof f != "string") {
-        return false
-    }
-    var h = "Arial";
-    if (f.toLowerCase() == h.toLowerCase()) {
-        return true
-    }
-    var e = "a";
-    var d = 100;
-    var a = 100,
-        i = 100;
-    var c = document.createElement("canvas");
-    var b = c.getContext("2d");
-    c.width = a;
-    c.height = i;
-    b.textAlign = "center";
-    b.fillStyle = "black";
-    b.textBaseline = "middle";
-    var g = function (j) {
-        b.clearRect(0, 0, a, i);
-        b.font = d + "px " + j + ", " + h;
-        b.fillText(e, a / 2, i / 2);
-        var k = b.getImageData(0, 0, a, i).data;
-        return [].slice.call(k).filter(function (l) {
-            return l != 0
-        })
+      return width;
     };
-    return g(h).join("") !== g(f).join("")
-}
 
-function isSupportFontFamily2(font) {
-  var width;
-  var body = document.body;
+    var monoWidth  = getWidth('monospace');
+    var serifWidth = getWidth('serif');
+    var sansWidth  = getWidth('sans-serif');
 
-  var container = document.createElement('span');
-  container.innerHTML = Array(100).join('wi');
-  container.style.cssText = [
-    'position:absolute',
-    'width:auto',
-    'font-size:128px',
-    'left:-99999px'
-  ].join(' !important;');
+    return monoWidth !== getWidth(font + ',monospace') || sansWidth !== getWidth(font + ',sans-serif') || serifWidth !== getWidth(font + ',serif');
+  }
 
-  var getWidth = function (fontFamily) {
-    container.style.fontFamily = fontFamily;
 
-    body.appendChild(container);
-    width = container.clientWidth;
-    body.removeChild(container);
-
-    return width;
-  };
-  var monoWidth  = getWidth('monospace');
-  var serifWidth = getWidth('serif');
-  var sansWidth  = getWidth('sans-serif');
-
-  return monoWidth !== getWidth(font + ',monospace') ||
-      sansWidth !== getWidth(font + ',sans-serif') ||
-      serifWidth !== getWidth(font + ',serif');
-}
-
-  // testCSS()
-
-  // testEvent('click')
-  // testEvent('none')
-
-  // testWindowObject()
-
-  // testApi
-
-  console.info('check font', isSupportFontFamily('PingFangSC-Regular'));
-  console.info('check font', isSupportFontFamily('Microsoft Yahei'));
-  console.info('check font', isSupportFontFamily2('PingFangSC-Regular'));
-  console.info('check font', isSupportFontFamily2('Microsoft Yahei'));
+  console.info('isSupport PingFangSC-Regular', isSupportFontFamily('PingFangSC-Regular'));
+  console.info('isSupport Microsoft Yahei', isSupportFontFamily('Microsoft Yahei'));
 
 
 
