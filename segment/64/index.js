fdb92931f6f921d43a1b5be37dc0fec51f1936d5
thy <972143891@qq.com>
2020-01-28 11:05:11 +0800

    feat: write test

--- segment/64/index.js
+++ segment/64/index.js
@@ -6,83 +6,30 @@ window.onload = function() {
   canvasEle.setAttribute('class','bg-canvas');
   var canvasContext = canvasEle.getContext('2d');
 
-  // var img = new Image();
-  // img.crossOrigin = "Anonymous";
-  // img.src = './poster.jpeg';
-  // img.onload = function() {
-  //   canvasEle.drawImage(img,0,0);
-  // }
 
-  // var img  = document.querySelector('#image');
-  // canvasContext.drawImage(img,0,0,248,415);
-  var rectProperty = {context:canvasContext,x:0, y:0, width:248, height:415,fillStyle:'rgba(0,0,0,0.5)'}
-  Util.CANVAS.drawRect(rectProperty);
-
-  canvasContext.globalCompositeOperation = "destination-out";
-  var deviceType = Util.getDeviceType();
-  // console.info('deviceType',deviceType);
-  function getErasePercentage() {
+  var img  = document.querySelector('#image');
+  canvasContext.drawImage(img,0,0,248,415);
     var imageData = canvasContext.getImageData(0,0,248,415);
     var colorDataArr = imageData.data;
     var colorDataArrLen = colorDataArr.length;
-    var eraseArea = [];
-    for(var i = 0; i < colorDataArrLen; i += 4) {
-      // 严格上来说，判断像素点是否透明需要判断该像素点的a值是否等于0，
-      if(colorDataArr[i + 3] < 64) {
-        eraseArea.push(colorDataArr[i + 3]);
+    for(var i = 0; i < colorDataArrLen; i ++) {
+      // 大于 128 置为 1，反之置为0
+      if (i%4) {
+        if(colorDataArr[i] < 128) {
+          colorDataArr[i] = 0;
+        } else {
+          colorDataArr[i] = 1;
+        }
       }
-    }
-    var divResult = eraseArea.length / (colorDataArrLen/4);
-    var pointIndex = String(divResult).indexOf('.');
-    if (pointIndex>-1) {
-      divResult = String(divResult).slice(0,pointIndex+5);
-    }
-    return (Number(divResult)*100).toFixed(2);
 
-  }
-  var isStartClear = false;
-  if (deviceType === 'phone') {
+    }
+    canvasContext.putImageData(imageData,0,0,248,415);
 
-  }
-  if (deviceType === 'pc') {
-    canvasEle.onmousedown = function (ev) {
-      console.info('onmousedown');
-      isStartClear = true;
-    }
-    canvasEle.onmouseup = function () {
-      console.info('onmouseup');
-      console.info('像素百分比：',getErasePercentage());
-      isStartClear = false;
-    }
-    canvasEle.onmousemove = function (event) {
-      event.stopPropagation();
-      if (isStartClear == false) return;
-      //清空像素,根据当前所在点
-      var centerX = event.clientX - canvasEle.offsetLeft;
-      var centerY = event.clientY - canvasEle.offsetTop;
-      var radius = 8;
-      canvasContext.beginPath();
-      canvasContext.arc(centerX, centerY, radius, 0, Math.PI * 2);
-      canvasContext.fill();
-    }
-  }
 
   document.body.appendChild(canvasEle);
 
-  document.querySelector('#clear').onclick = function() {
-    var centerX = 248/2;
-    var centerY = 415/2;
-    var maxRadius = Math.sqrt( Math.pow(centerX,2) + Math.pow(centerY,2) ) + 1;
-    var radius = 10;
-    canvasContext.beginPath();
-    var count = setInterval(() => {
-      if (radius>maxRadius) {
-        clearInterval(count);
-      }
-      radius+=3;
-      canvasContext.arc(centerX, centerY, radius, 0, Math.PI * 2);
-      canvasContext.fill();
-    }, 10);
+  // document.querySelector('#clear').onclick = function() {
 
-  }
+
+  // }
 }
