45e29759bc41e20ae3267c3b16c39a45f416d212
thy <972143891@qq.com>
2020-02-03 04:16:52 +0800

    docs: write 46.md

--- blog/46/rect.js
+++ blog/46/rect.js
@@ -63,10 +63,10 @@ var page = {
     };
 
     var drawFrame = function() {
-      if (rect1.x < rect2.x + rect2.width &&
-        rect1.x + rect1.width > rect2.x &&
-        rect1.y < rect2.y + rect2.height &&
-        rect1.height + rect1.y > rect2.y) {
+      if (rect1.x <= rect2.x + rect2.width &&
+        rect1.x + rect1.width >= rect2.x &&
+        rect1.y <= rect2.y + rect2.height &&
+        rect1.height + rect1.y >= rect2.y) {
         // collision detected!
         that.isCollision = false;
         clearTimeout(that.drawTimeOut);
