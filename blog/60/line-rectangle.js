9987c9358b896837b4778b86f491ecfec4dbc135
thy <972143891@qq.com>
2020-07-22 09:43:31 +0800

    docs: modify

--- blog/60/line-rectangle.js
+++ blog/60/line-rectangle.js
@@ -149,7 +149,7 @@ const page = {
 
 
 window.onload = function() {
-  // Util.insertLink({title:'Collision Detection ：Rectangle',linkIndex: 60, type: 'blog'});
+  Util.insertLink({title:'Collision Detection ：Line',linkIndex: 61, type: 'blog'});
   Util.loading.show();
   page.init();
   // try {
