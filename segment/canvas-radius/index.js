0ec85eb6be7079189567c1b9984db7e2f7ebdc67
thy <972143891@qq.com>
2020-09-17 18:03:29 +0800

    feat: try canvas

---
+++ segment/canvas-radius/index.js
@@ -0,0 +1,6 @@
+
+window.onload = function() {
+  // insertLink({title:'Canvas 绘制 1 px 直线模糊（非高清屏）的问题',linkIndex: 86});
+  var canvasHD = Util.CANVAS.createElement(300,90);
+  var canvasHDContext = canvasHD.getContext('2d');
+}
