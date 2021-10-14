24c64844477b9d122a9b32c449bd984326d9e87e
thy <972143891@qq.com>
2021-10-14 09:41:36 +0800

    feat: add config in 87

--- segment/87/index.js
+++ segment/87/index.js
@@ -1,5 +1,9 @@
 window.onload = function () {
   const page = {
+    initAngle: 0.1,
+    a: 100,
+    accelerationAngle: 0.1,
+    circleNum: 20,
     canvasObj: {},
     lineAttrs: {
       points: [],
@@ -13,17 +17,18 @@ window.onload = function () {
       document.querySelector("#archimedeanSpiral").appendChild(canvasObj.node);
       this.canvasObj = canvasObj;
       this.draw();
+      this.pageEvent();
     },
     draw: function () {
-      let a = 100,
-        angle = 0.1;
+      let a = this.a,
+        angle = this.initAngle;
       let points = [],
         x = 0,
         y = 0;
-      const acceleration = 0.1,
-        circleNum = 20;
+      const acceleration = this.accelerationAngle,
+        circleNum = this.circleNum;
       const { lineAttrs, canvasObj } = this;
-      let { width, height, translate, line } = canvasObj;
+      let { width, height, translate, line, clear, resetTransform } = canvasObj;
       while (angle <= circleNum * 2 * Math.PI) {
         const angleSqrt = Math.sqrt(angle);
         x = (a / angleSqrt) * Math.cos(angle);
@@ -31,12 +36,50 @@ window.onload = function () {
         points.push([x, y]);
         angle = MathLab.plus(angle, acceleration);
       }
-      console.info("points", points);
+      // console.info("points", points);
+      clear();
       translate(width / 4, height / 2);
       line({
         ...lineAttrs,
         points: points,
       });
+      resetTransform();
+    },
+    pageEvent: function () {
+      document.querySelector("#apply").onclick = () => {
+        const formObj = document.mathAttr;
+        const a = formObj.attrA.value;
+        // const b = formObj.attrB.value;
+        const initAngle = formObj.initAngle.value;
+        const accelerationAngle = formObj.accelerationAngle.value;
+        const circleNum = formObj.circleNum.value;
+        if ([a, initAngle, circleNum, accelerationAngle].includes("")) {
+          alert("所有值不能为空");
+          return;
+        }
+        if (a == 0) {
+          alert("a 值无效 ");
+          return;
+        }
+        if (initAngle == 0) {
+          alert("初始角度 值无效 ");
+          return;
+        }
+        if (accelerationAngle == 0) {
+          alert("递增角度 值无效 ");
+          return;
+        }
+        if (circleNum < 1) {
+          alert("圈数 值无效");
+          return;
+        }
+        this.a = Number(a);
+        // this.b = Number(b);
+        this.initAngle = Number(initAngle);
+        this.accelerationAngle = Number(accelerationAngle);
+        this.circleNum = Number(circleNum);
+        this.draw();
+      };
     },
   };
 
