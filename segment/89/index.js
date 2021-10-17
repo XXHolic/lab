6dcb7ff572f905481b3c047f02f66f4aa83c870f
thy <972143891@qq.com>
2021-10-17 08:59:03 +0800

    feat: add test for 89

---
+++ segment/89/index.js
@@ -0,0 +1,85 @@
+window.onload = function () {
+  const page = {
+    initStart: 0,
+    a: -100,
+    acceleration: 0.1,
+    max: 40,
+    canvasObj: {},
+    lineAttrs: {
+      points: [],
+      lineWidth: 1,
+      lineCap: "round",
+      strokeStyle: "rgba(0,148,255,1)",
+      fillStyle: "rgba(0,148,255,1)",
+    },
+    init: function () {
+      const canvasObj = new Canvas(400, 300);
+      canvasObj.attrs({ class: "demo-canvas" });
+      document.querySelector("#bicorn").appendChild(canvasObj.node);
+      this.canvasObj = canvasObj;
+      this.draw();
+      this.pageEvent();
+    },
+    draw: function () {
+      let a = this.a,
+        start = this.initStart;
+      let points = [],
+        x = 0,
+        y = 0;
+      const acceleration = this.acceleration,
+        max = this.max;
+      const { lineAttrs, canvasObj } = this;
+      let { width, height, translate, line, clear, resetTransform } = canvasObj;
+      while (start <= max) {
+        x = a * Math.sin(start);
+        const yNumerator =
+          a * Math.pow(Math.cos(start), 2) * (2 + Math.cos(start));
+        const yDenominator = 3 + Math.pow(Math.sin(start), 2);
+        y = yNumerator / yDenominator;
+        points.push([x, y]);
+        start = MathLab.plus(start, acceleration);
+      }
+      // console.info("points", points);
+      clear();
+      translate(width / 2, height / 2);
+      line({
+        ...lineAttrs,
+        points: points,
+      });
+      resetTransform();
+    },
+    pageEvent: function () {
+      document.querySelector("#apply").onclick = () => {
+        const formObj = document.mathAttr;
+        const a = formObj.attrA.value;
+        // const b = formObj.attrB.value;
+        // const initStart = formObj.initStart.value;
+        const acceleration = formObj.acceleration.value;
+        const max = formObj.max.value;
+        if ([a, max, acceleration].includes("")) {
+          alert("所有值不能为空");
+          return;
+        }
+        if (a == 0) {
+          alert("a 值无效 ");
+          return;
+        }
+        if (acceleration == 0) {
+          alert("递增值 值无效 ");
+          return;
+        }
+        if (max < 1) {
+          alert("变化最大值 值无效");
+          return;
+        }
+        this.a = Number(a);
+        this.acceleration = Number(acceleration);
+        this.max = Number(max);
+        this.draw();
+      };
+    },
+  };
+
+  page.init();
+  insertLink({ title: "JavaScript 数学曲线—星形线(Astroid)", linkIndex: 106 });
+};
